9c5c08e581886074f35990b5220ad05ef5433c44
Chandani Yadav <chandani.yadav@newvisionsoftware.com>
2023-06-13 18:34:08 +0530

    first commit

---
+++ Controllers/ChatGPTController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+using OpenAI_API;
+
+namespace ChatGpt_back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatGPTController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAnswer(string input)
+        {
+            string apiKey = "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";
+            string response = "";
+            OpenAIAPI openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = input;
+            completion.Model = "text-davinci-003";
+            completion.MaxTokens = 4000;
+            var output = await openai.Completions.CreateCompletionAsync(completion);
+            if (output != null)
+            {
+                foreach (var item in output.Completions)
+                {
+                    response = item.Text;
+                }
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest("Not found");
+            }
+        }
+    }
+}
